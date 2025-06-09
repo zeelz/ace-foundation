1cf19378c1ff0028950fd94c24777d631d6892ee
Keme <keme.kenneth@gmail.com>
2025-06-09 14:16:24 +0100

    Update contact form to submit

--- app/contact/action.ts
+++ app/contact/action.ts
@@ -1,54 +1,55 @@
 "use server"
 
-import nodemailer from "nodemailer"
+// import nodemailer from "nodemailer"
 // import { MailtrapTransport } from "mailtrap"
-// const { MailtrapClient } = require("mailtrap");
+
+
+const Nodemailer = require("nodemailer");
+import { MailtrapTransport } from "mailtrap";
+
 
 export const FormAction = async (formData: FormData) => {
-    // console.log({formData})
 
-    // const res = await fetch("http://localhost:3300/api")
-    // const data = await res.json()
-    // console.log({data})
+    const firstname = formData.get("firstname")
+    const lastname = formData.get("lastname")
+    const email = formData.get("email")
+    const phone = formData.get("phone")
+    const message = formData.get("message")
 
-    
+    const mailBody = `
+        \nName: ${firstname} ${lastname}
+        \nEmail: ${email}
+        \nPhone: ${phone}
+        \nMessage: ${message}
+    `
+
+const TOKEN = process.env["NEXT_PUBLIC_TOKEN"] || "";
+
+const transport = Nodemailer.createTransport(
+  MailtrapTransport({
+    token: TOKEN,
+  })
+);
+
+const sender = {
+  address: "hello@acefoundation.com.ng",
+  name: "Ace Foundation",
+};
+const recipients = [
+  "hello@acefoundation.com.ng",
+];
+
+transport
+  .sendMail({
+    from: sender,
+    to: recipients,
+    subject: "Message from Contact Form",
+    text: mailBody,
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
 
 
-// Looking to send emails in production? Check out our Email API/SMTP product!
 
-    const TOKEN = "92b4d5fba89f4bf6a21866680553377f";
-    const SENDER_EMAIL = "hello@example.com";
-    const RECIPIENT_EMAIL = "keme.kenneth@gmail.com";
-
-    
-
-    // Create a transporter object
-    const transporter = nodemailer.createTransport({
-    // host: 'live.smtp.mailtrap.io',
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 587,
-    secure: false, // use SSL
-    auth: {
-        user: '25786726835a80',
-        pass: '7097809abd3d86',
-    }
-    });
-
-    // Configure the mailoptions object
-    const mailOptions = {
-        from: SENDER_EMAIL,
-        to: RECIPIENT_EMAIL,
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    };
-
-    // Send the email
-    transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log('Error:', error);
-    } else {
-        console.log('Email sent:', info.response);
-    }
-    });
 
 }
